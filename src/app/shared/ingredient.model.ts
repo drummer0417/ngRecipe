5491ad9a6329b77ee67ba7de8c32ffdd2fc9954a
Hans van Meurs <hans@androidappfactory.nl>
2020-01-18 17:33:40 +0100

    Added templates and the Ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient{
+
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amoumt: amount){
+    //     this.name = name;
+    //     this.amount = amoumt;
+    // }
+
+    // the constructor below replaces all the above commented lines. Cool!
+    constructor(public name: string, public amoumt: number){};
+
+    
+} 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  public ingredients: Ingredient[] = [
+    new Ingredient("Appe", 5),
+    new Ingredient('Banana', 3)
+  ];
 
   constructor() { }
 
